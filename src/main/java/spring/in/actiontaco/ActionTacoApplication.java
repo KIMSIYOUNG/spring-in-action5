24e185de0735fff990cf03364a2bb8667d715ab7
KIMSIYOUNG <tldud2404@gmail.com>
2020-06-28 11:18:23 +0900

    feat : project init

---
+++ src/main/java/spring/in/actiontaco/ActionTacoApplication.java
@@ -0,0 +1,13 @@
+package spring.in.actiontaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionTacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionTacoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/in/actiontaco/ActionTacoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.in.actiontaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionTacoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
